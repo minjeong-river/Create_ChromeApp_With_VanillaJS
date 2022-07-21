910fe1bc93dd67d02f0586fba22243cb85ebd4b9
mjkang <mj.developer.kr@gmail.com>
2022-07-21 22:09:54 +0900

    Update app.js

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,13 +1,15 @@
+let body = document.querySelector("body");
 let title = document.querySelector(".title");
 
 function mouseEnter() {
     title.innerHTML = "Mouse is here! 😊";
+    body.style.background = "#ffe0de";
 }
 
 function mouseLeave() {
     title.innerHTML = "Mouse is gone! 😂";
+    body.style.background = "#e0f4fe";
 }
-
 
 title.addEventListener("mouseenter", mouseEnter);
 title.addEventListener("mouseleave", mouseLeave);
