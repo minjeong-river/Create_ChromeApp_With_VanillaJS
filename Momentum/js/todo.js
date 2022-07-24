1403eda54305cbb77a56cd598aaa470f0e705843
mjkang <mj.developer.kr@gmail.com>
2022-07-24 22:24:41 +0900

    Update Deleting To Dos

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,14 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement.innerText;
+    console.log(li);
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
+
     span.innerText = newTodo;
 
-    toDoList.appendChild(li);
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button)
     
+    toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
