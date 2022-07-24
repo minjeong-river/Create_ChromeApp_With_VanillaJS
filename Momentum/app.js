d92178b7ee08d2b656f8ce81b602b3d04d8fc591
mjkang <mj.developer.kr@gmail.com>
2022-07-24 11:45:04 +0900

    Update Saving Username

--- Momentum/app.js
+++ Momentum/app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAMES = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAMES);
-    // greeting.innerText = "Hello!" + username;
-    greeting.innerText = `Hello ${username}`
+    localStorage.setItem("USERNAME_KEY", username);
+    greeting.classList.remove(HIDDEN_CLASSNAMES);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello! ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAMES);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAMES);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    paintGreetings(savedUsername);
+}
