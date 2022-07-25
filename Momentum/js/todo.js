cbd2345213c466df789a20279eb6fe3900ec70eb
mjkang <mj.developer.kr@gmail.com>
2022-07-25 22:57:11 +0900

    Update Deleting To Dos part One

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -13,14 +13,17 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
+
     const span = document.createElement("span");
 
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
 
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -36,8 +39,14 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+
+    const newTodoObj = {
+        id:Date.now(),
+        text:newTodo,
+    }
+
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
